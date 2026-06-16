854bd5f19caebc222497611b60a199c967ad8b65
Hydra-nt1 <nick.groth@co-orga.de>
2026-06-16 17:00:18 +0200

    SW: skipWaiting + auto-update

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
-const CACHE = 'fittracker-v21';
+const CACHE = 'fittracker-v22';
 const FILES = ['./', './index.html', './css/style.css', './js/db.js', './js/app.js', './manifest.json', './icon.svg'];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
