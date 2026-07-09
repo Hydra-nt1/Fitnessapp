caa3a6819776082d178059da19885b7dbaced3b2
Nick Groth | Co-Orga GmbH <nick.groth@co-orga.de>
2026-07-09 10:44:35 +0200

    Bump cache v42

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fittracker-v41';
+const CACHE = 'fittracker-v42';
 const FILES = ['./', './index.html', './css/style.css', './js/db.js', './js/app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
